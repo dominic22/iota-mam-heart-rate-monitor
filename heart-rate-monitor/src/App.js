a2df689dc899fa40f3016d18a390f7a764cf55a5
dominic22 <nexus.domi123@gmail.com>
2019-11-21 22:37:37 +0100

    Add router, fetch and log tangle messages

--- heart-rate-monitor/src/App.js
+++ heart-rate-monitor/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { asciiToTrytes } from '@iota/converter';
+import { BrowserRouter as Router, Route, Switch, useParams } from 'react-router-dom';
+import { trytesToAscii } from '@iota/converter';
 import Mam from '@iota/mam';
 
 const mode = 'restricted';
@@ -9,25 +10,54 @@ const secretKey = 'SECRETBIG'; // secret always upper case!
 const provider = 'https://nodes.devnet.iota.org';
 let mamState = Mam.init(provider);
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/:root">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+// You can think of these components as "pages"
+// in your app.
+const logData = data => console.log('Fetched and parsed', JSON.parse(trytesToAscii(data)), '\n')
+
+function Home() {
+  const { root } = useParams();
+  Mam.fetch(root, mode, secretKey, logData);
+
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p>
+        Current root: {root}
+      </p>
+    </header>
+  );
+}
+
+function About() {
+  return (<div>
+    <h2>About</h2>
+  </div>)
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
