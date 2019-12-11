1f6ecdec40348dec165b5df871ba54eb5be96d61
dominic22 <nexus.domi123@gmail.com>
2019-12-11 21:27:21 +0100

    update monitor ui

--- heart-rate-monitor/src/App.js
+++ heart-rate-monitor/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.png';
-import ChartViewComponent, {  addData} from './chart/Chart'
+import ChartViewComponent, { addData } from './chart/Chart'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, useParams } from 'react-router-dom';
 import { trytesToAscii } from '@iota/converter';
 import Mam from '@iota/mam';
 import moment from 'moment';
 import 'moment/locale/de';
+
 const mode = 'restricted';
 const secretKey = 'SECRETBIG'; // secret always upper case!
 const provider = 'https://nodes.devnet.iota.org';
@@ -15,11 +16,11 @@ Mam.init(provider);
 export default function App() {
   return (
     <Router>
-        <header className="app-header">
+      <header className="app-header">
         <Switch>
           <Route exact path="/">
             <header className="app-header">
-            <p>You have not specified a root. Please call the URL as following: localhost:3001/ROOT</p>
+              <p>You have not specified a root. Please call the URL as following: localhost:3001/ROOT</p>
             </header>
           </Route>
           <Route exact path="/:root">
@@ -38,17 +39,18 @@ const logData = encodedData => {
   const data = JSON.parse(trytesToAscii(encodedData));
   console.log('Fetched and parsed', data, '\n')
   console.log('data ', data.heartRate)
-  const date = new Date(data.timestamp);
 
   moment.locale('de');
   let dateString = moment(data.timestamp).format('LTS');
 
-  if(!prevDate || date.getDate() !== prevDate.getDate() || date.getMonth() !== prevDate.getMonth() || date.getFullYear() !== prevDate.getFullYear()) {
-      dateString = moment(data.timestamp).format('MMMM Do YYYY, hh:mm:ss');
-      prevDate = date;
-  }
-  addData(dateString, data.heartRate);
-}
+  // if (!prevDate || date.getDate() !== prevDate.getDate() || date.getMonth() !== prevDate.getMonth() || date.getFullYear() !== prevDate.getFullYear()) {
+  //   dateString = moment(data.timestamp).format('MM.DD.YYYY');
+  //   prevDate = date;
+  // }
+  const date = moment(data.timestamp).format('MM.DD.YYYY');
+
+  addData(dateString, data.heartRate, date);
+};
 
 async function pullTangleData(root) {
   const fetched = await Mam.fetch(root, mode, secretKey, logData);
--- heart-rate-monitor/src/chart/Chart.js
+++ heart-rate-monitor/src/chart/Chart.js
@@ -4,67 +4,151 @@ import Chart from 'chart.js';
 
 var chart;
 
-export function initializeChart(data, labels) {
-    var ctx = document.getElementById('heart-rate-chart').getContext('2d');
-    chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels,
-            datasets: [{
-                label: 'Heart Rate',
-                data,
-                borderColor: [
-                    'rgba(223, 71, 50, 1)'
-                ],
-                backgroundColor: [
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive:true,
-            legend : {
-                display: false,
-            },
-            scales: {
-                xAxes:[{
-                    ticks:{
-                        fontColor:'#ffffff',
-                    },
-                }],
-                yAxes: [{
-                    ticks: {
-                        fontColor: '#ffffff',
-                        beginAtZero: true
-                    }
-                }]
-            },
-            tooltips: {
-                intersect: false
-            }
-        }
+const customTooltips = function (tooltip) {
+  // Tooltip Element
+  var tooltipEl = document.getElementById('chartjs-tooltip');
+
+  if (!tooltipEl) {
+    tooltipEl = document.createElement('div');
+    tooltipEl.id = 'chartjs-tooltip';
+    tooltipEl.innerHTML = '<table></table>';
+    this._chart.canvas.parentNode.appendChild(tooltipEl);
+  }
+
+  // Hide if no tooltip
+  if (tooltip.opacity === 0) {
+    tooltipEl.style.opacity = 0;
+    return;
+  }
+
+  // Set caret Position
+  tooltipEl.classList.remove('above', 'below', 'no-transform');
+  if (tooltip.yAlign) {
+    tooltipEl.classList.add(tooltip.yAlign);
+  } else {
+    tooltipEl.classList.add('no-transform');
+  }
+
+  function getBody(bodyItem) {
+    return bodyItem.lines;
+  }
+
+  // Set Text
+  if (tooltip.body) {
+    var titleLines = tooltip.title || [];
+    var bodyLines = tooltip.body.map(getBody);
+
+    let innerHtml = '<thead>';
+
+    titleLines.forEach(function (title) {
+      innerHtml += '<tr><th>' + title + '</th></tr>';
     });
+    innerHtml += '</thead><tbody>';
+
+    bodyLines.forEach(function (body, i) {
+      const colors = tooltip.labelColors[i];
+      let style = 'background:' + colors.backgroundColor;
+      style += '; border-color:' + colors.borderColor;
+      style += '; border-width: 2px';
+      style += '; font-size: 26px';
+      const span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+      innerHtml += '<tr><td>' + span + body + '</td></tr>';
+    });
+    innerHtml += '</tbody>';
+
+    var tableRoot = tooltipEl.querySelector('table');
+    tableRoot.innerHTML = innerHtml;
+  }
+
+  var positionY = this._chart.canvas.offsetTop;
+  var positionX = this._chart.canvas.offsetLeft;
+
+  // Display, position, and set styles for font
+  tooltipEl.style.opacity = 1;
+  tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+  tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+  tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+  tooltipEl.style.fontSize = tooltip.bodyFontSize + 'px';
+  tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
+  tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+};
+
+export function initializeChart(data, labels) {
+  var ctx = document.getElementById('heart-rate-chart').getContext('2d');
+  chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels,
+      datasets: [{
+        label: 'Heart Rate',
+        data,
+        borderColor: [
+          'rgba(223, 71, 50, 1)'
+        ],
+        backgroundColor: ['rgba(0, 0, 0, 0.2)'],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: '',
+        padding: 20,
+        fontColor: '#ffffff',
+        fontSize: 22,
+        fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+        fontStyle: 'normal'
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            fontColor: '#ffffff',
+          },
+        }],
+        yAxes: [{
+          ticks: {
+            fontColor: '#ffffff',
+            beginAtZero: true
+          }
+        }]
+      },
+      tooltips: {
+        enabled: false,
+        intersect: false,
+        custom: customTooltips
+      }
+    }
+  });
 }
 
-export function addData(label, data) {
-    console.log('ADD DATA ', label, data)
-    if(!chart) {
-        return;
-    }
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
+export function addData(label, data, title) {
+  console.log('ADD DATA ', label, data)
+  if (!chart) {
+    return;
+  }
+  if(title) {
+    chart.options.title.text = title;
+  }
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+  chart.update();
 }
 
 export default function ChartViewComponent(props) {
-    useEffect(() => {
-       initializeChart(props.data, props.labels);
-      });
-    return (
-        <div className="chart-container">
-            <canvas id="heart-rate-chart" height="400" style={ {width: "100%"}}></canvas>
-        </div>
-    );
+  useEffect(() => {
+    initializeChart(props.data, props.labels);
+  });
+  return (
+    <div className="chart-wrapper">
+      <div className="chart-container">
+        <canvas id="heart-rate-chart" height="400" style={{ width: '100%' }}></canvas>
+      </div>
+    </div>
+  );
 }
