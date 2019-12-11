6efc7e104054fee509802836ed624459d523d440
dominic22 <nexus.domi123@gmail.com>
2019-12-11 15:00:50 +0100

    Implement furhter netlify server instructions

--- heart-rate-backend/src/index.js
+++ heart-rate-backend/src/index.js
@@ -1,22 +1,6 @@
-var express = require('express');
-
 const port = 8081;
-const app = express();
 
-app.get('/', function (req, res) {
-  const { heartRate } = req.query;
-  if (heartRate) {
-    // TODO send heart rate to tangle!
-    console.log('send heart rate to tangle!', heartRate);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Sent heart rate to tangle ' + heartRate + ' .</h1>');
-    res.end();
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>No heart rate received. Please add query parameter heartRate to your request.</h1>');
-    res.end();
-  }
-});
+const app = require('./server');
 
 app.listen(port, () => {
   console.log('app is running on port ', port);
---
+++ heart-rate-backend/src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { heartRate } = req.query;
+  console.log('ree ', req);
+  if (heartRate) {
+    // TODO send heart rate to tangle!
+    console.log('send heart rate to tangle!', heartRate);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Sent heart rate to tangle ' + heartRate + ' .</h1>');
+    res.end();
+  } else {
+    console.log('no heart rate parameter provided');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<p>No heart rate received. Please add query parameter heartRate to your request.</p>' +
+      '<p>You might have to call the url as domain.com/.netlify/functions/server?heartRate=53</p>');
+    res.end();
+  }
+});
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
